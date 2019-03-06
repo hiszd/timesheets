e6303d1c79720988228f80ffbaca6e92e5179f49
Zion Koyl <hiszd1@gmail.com>
2019-03-06 14:23:25 +0000

    06MAR19-0926

--- app.js
+++ app.js
@@ -12,37 +12,37 @@ const port = 5000;
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "parts",
-    port: 3306
+	host: "localhost",
+	user: "root",
+	password: "password",
+	database: "parts",
+	port: 3306
 });
 
 // connect to database
 db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
-    var tablecheckquery = "SHOW TABLES LIKE 'items';";
-    db.query(tablecheckquery, (err, result) => {
-        if (err) {
-            throw err;
-        } else {
-	    if(result!="") {
-		console.log("Table Already Exists.");
-	    } else {
-		var query = "CREATE TABLE IF NOT EXISTS `items` ( `id` int(32) NOT NULL AUTO_INCREMENT, `station_id` varchar(255) NOT NULL, `status` varchar(255) NOT NULL, `qty` int(32) NOT NULL, `manufacturer` varchar(255) NOT NULL, `part_number` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
-		db.query(query, (err, result) => {
-                    if (err) {
-                        throw err;
-                    }
-                    console.log("Table Created.");
-                });
-	    }
+	if (err) {
+		throw err;
 	}
-    });
+	console.log('Connected to database');
+	var tablecheckquery = "SHOW TABLES LIKE 'items';";
+	db.query(tablecheckquery, (err, result) => {
+		if (err) {
+			throw err;
+		} else {
+			if(result!="") {
+				console.log("Table Already Exists.");
+			} else {
+				var query = "CREATE TABLE IF NOT EXISTS `items` ( `id` int(32) NOT NULL AUTO_INCREMENT, `station_id` varchar(255) NOT NULL, `status` varchar(255) NOT NULL, `qty` int(32) NOT NULL, `manufacturer` varchar(255) NOT NULL, `part_number` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
+				db.query(query, (err, result) => {
+					if (err) {
+					throw err;
+					}
+					console.log("Table Created.");
+				});
+			}
+		}
+	});
 });
 global.db = db;
 
@@ -65,5 +65,5 @@ app.post('/edit/:id', editPart);
 
 // set the app to listen on the port
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+	console.log(`Server running on port: ${port}`);
 });
