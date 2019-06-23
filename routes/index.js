2ab6ea3715170e63cc1214d461f1497f2da605c2
Zion Koyl <hiszd1@gmail.com>
2019-06-23 17:27:54 -0400

    06/23/19-1727

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+const {conditionOutput} = require('../lib/lib');
 module.exports = {
     getHomePage: (req, res, next) => {
         let query = "SELECT * FROM `tasks` ORDER BY bucket ASC"; // query database to get all the players
-
+	res.charset = "UTF-8";
+	res.set({ 'content-type': 'text/html; charset=utf-8' });
         // execute query
         db.query(query, (err, result) => {
             if (err) {
@@ -11,6 +13,12 @@ module.exports = {
 		});
 		console.log("index.js-"+err);
             } else {
+		result.forEach(function(itm, i) {
+		    itm.bucket = conditionOutput(itm.bucket);
+		    itm.task = conditionOutput(itm.task);
+		    itm.description = conditionOutput(itm.description);
+		    itm.notes = conditionOutput(itm.notes);
+		});
 		res.render('index.ejs', {
                     title: 'Welcome to SoliDesk | View Tasks'
                     ,tasks: result
