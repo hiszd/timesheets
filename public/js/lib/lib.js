72aea053d13e4cbd66590edba596292f90b2fa25
Zion Koyl <hiszd1@gmail.com>
2019-09-06 23:10:48 -0400

    09/06/19-2311

---
+++ public/js/lib/lib.js
@@ -0,0 +1,11 @@
+var makeObserver = (targetNode, callback) => {
+	// Options for the observer (which mutations to observe)
+	const config = { attributes: true };
+
+	// Create an observer instance linked to the callback function
+	const observer = new MutationObserver(callback);
+
+	// Start observing the target node for configured mutations
+	// observer.observe(targetNode, config);
+	return observer;
+}
